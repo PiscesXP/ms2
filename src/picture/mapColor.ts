8764c6f4224a4f1f0623ee3e1f0204573debac4e
Zhenxi <PiscesXP@outlook.com>
2021-02-24 13:39:22 +0800

    test: 图片材质只用混凝土

--- src/picture/mapColor.ts
+++ src/picture/mapColor.ts
@@ -1,8 +1,9 @@
 import { ColorPool } from "./colorPool";
-import {PNG} from "pngjs";
+import { PNG } from "pngjs";
 
 const mapColor = new ColorPool();
 // 数据值参考 https://minecraft-zh.gamepedia.com/地图物品格式
+/*
 mapColor.addColor(127, 178, 56, "grass");
 mapColor.addColor(247, 233, 163, "planks", 2);
 mapColor.addColor(199, 199, 199, "web");
@@ -17,6 +18,7 @@ mapColor.addColor(112, 112, 112, "smooth_stone");
 mapColor.addColor(64, 64, 255, "water");
 mapColor.addColor(143, 119, 72, "planks", 0);
 mapColor.addColor(255, 252, 245, "quartz_block");
+*/
 mapColor.addColor(216, 127, 51, "concrete", 1);
 mapColor.addColor(178, 76, 216, "concrete", 2);
 mapColor.addColor(102, 153, 216, "concrete", 3);
@@ -32,6 +34,7 @@ mapColor.addColor(102, 76, 51, "concrete", 12);
 mapColor.addColor(102, 127, 51, "concrete", 13);
 mapColor.addColor(153, 51, 51, "concrete", 14);
 mapColor.addColor(25, 25, 25, "concrete", 15);
+/*
 mapColor.addColor(250, 238, 77, "gold_block");
 mapColor.addColor(92, 219, 213, "diamond_block");
 mapColor.addColor(74, 128, 255, "lapis_block");
@@ -54,6 +57,7 @@ mapColor.addColor(76, 50, 35, "stained_hardened_clay", 12);
 mapColor.addColor(76, 82, 42, "stained_hardened_clay", 13);
 mapColor.addColor(142, 60, 46, "stained_hardened_clay", 14);
 mapColor.addColor(37, 22, 16, "stained_hardened_clay", 15);
+*/
 
 /**
  * 在指定坐标处绘制一副像素画
@@ -63,7 +67,13 @@ mapColor.addColor(37, 22, 16, "stained_hardened_clay", 15);
  * @param png png图片
  * @param towards 基点为图片左上角，确定朝向
  */
-export const mapColorConvert = (x: number, y: number, z: number, png: PNG, towards: string) => {
+export const mapColorConvert = (
+  x: number,
+  y: number,
+  z: number,
+  png: PNG,
+  towards: string
+) => {
   const result = [];
   const colorArray = mapColor.fitPicture(png);
   for (let i = 0; i < colorArray.length; i++) {
@@ -71,24 +81,28 @@ export const mapColorConvert = (x: number, y: number, z: number, png: PNG, towar
       const color = colorArray[i][j];
       let position: number[] = [];
       if (towards === "south") {
-        position = [x-j, y, z-i];
+        position = [x - j, y, z - i];
       } else if (towards === "west") {
-        position = [x+i, y, z-j];
+        position = [x + i, y, z - j];
       } else if (towards === "east") {
-        position = [x-i, y, z+j];
+        position = [x - i, y, z + j];
       } else {
-        position = [x+j, y, z+i];
+        position = [x + j, y, z + i];
       }
       if (color.id === "air") {
         continue;
       }
       if (color.id === "water") {
-        position[1] --;
+        position[1]--;
         result.push(`fill ${position.join(" ")} ${position.join(" ")} stone`);
-        position[1] ++;
+        position[1]++;
       }
-      result.push(`fill ${position.join(" ")} ${position.join(" ")} ${color.id} ${color.data}`);
+      result.push(
+        `fill ${position.join(" ")} ${position.join(" ")} ${color.id} ${
+          color.data
+        }`
+      );
     }
   }
   return result;
-}
+};
