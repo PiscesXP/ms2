0813e71712334e06e30a979695d8630f2dc08a00
Zhenxi <PiscesXP@outlook.com>
2021-02-24 14:59:19 +0800

    解析schema

--- src/picture/colorSchema/index.ts
+++ src/picture/colorSchema/index.ts
@@ -1,14 +1,30 @@
 import { DefaultSchema } from "./DefaultSchema";
 import { ConcreteOnlySchema } from "./ConcreteOnlySchema";
+import { ConcreteAndStainedClaySchema } from "./ConcreteAndStainedClaySchema";
+import { StainedClaySchema } from "./StainedClaySchema";
+import { ColorSchema } from "./ColorSchema";
 
 export { ColorSchema } from "./ColorSchema";
 
 const defaultSchema = new DefaultSchema();
 const concreteOnlySchema = new ConcreteOnlySchema();
+const concreteAndStainedClaySchema = new ConcreteAndStainedClaySchema();
+const stainedClaySchema = new StainedClaySchema();
 
-export const parseSchema = (schema = "") => {
-  if (/concrete/i.test(schema)) {
+/**
+ * 解析并返回对应的schema.
+ * */
+export const parseSchema = (schema = ""): ColorSchema => {
+  const concreteTest = /concrete/i.test(schema);
+  const stainedClayTest = /stainedclay/i.test(schema);
+  if (concreteTest && stainedClayTest) {
+    return concreteAndStainedClaySchema;
+  }
+  if (concreteTest) {
     return concreteOnlySchema;
+  }
+  if (stainedClayTest) {
+    return stainedClaySchema;
   }
   return defaultSchema;
 };
